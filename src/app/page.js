908ba7b180808c7256edeb11a5bdd912cd450f60
Mino <bryanyoba2000@gmail.com>
2024-02-09 22:04:26 -0500

    v1.3.1.2
    Nuevos Logos

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import Navbar from "../components/Navbar";
 import Home1 from "../components/Home"
 import Process from "../components/Process"
 
+
 export default function Home() {
   return (
    <>
     <Navbar/>
     <Home1/>
     <Process />
+
    </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Home(){
     return(
-        <a className="bg-[#f7feff]   flex flex-col items-center bg-gray-900   shadow md:flex-row  " >
-            <div className=" mx-20 md:mr-40 md:ml-40"> 
-                <img class="h-32 w-32 md:h-80 md:w-80 mx-auto" src="logo.png" alt="" />
-                <h1 className="text-2xl md:text-3xl ">En lavanderias Dany Tenemos de todo  para hacerte mas facil tu dia.</h1>
-                <h1 className="mt-10 md:mb-10">Con equipos nuevos para no maltratar tu ropa.</h1>
+        <a className="bg-[#f7feff]   flex flex-col items-center    shadow md:flex-row  " >
+            <div className=" mx-20 md:mr-40 md:ml-40 "> 
+                <img class="h-32 w-32 md:h-64 md:w-64 mx-auto mt-10 mb-10" src="logo3.png" alt="" />
+                <h1 className="text-2xl md:text-3xl h- ">En lavanderias Dany Tenemos de todo  para hacerte mas facil tu dia.</h1>
+                <h1 className="mt-10 md:mb-10">Con equipos de calidad para no maltratar tu ropa.</h1>
             </div>
 
             <div>
